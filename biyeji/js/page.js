08ab9ae2351d36463b3fb19a76a7e8b6994b4c64
feiyuhan <1191239053@qq.com>
2017-06-28 03:52:24 +0800

    毕业季刊

---
+++ biyeji/js/page.js
@@ -0,0 +1,292 @@
+/**
+ * @Author: lt
+ * @Date:   2017-06-27T10:17:29+08:00
+ * @Last modified by:   lt
+ * @Last modified time: 2017-06-28T03:09:46+08:00
+ */
+
+
+
+//
+//
+// var swiper1 = new Swiper('.swiper-container-horizontal', {
+//         pagination: '.swiper-pagination',
+//         paginationClickable: true,
+//         hashnav: true,
+//     });
+// var clientHeight = document.body.clientHeight;
+// var clientwidth = document.body.clientWidth;
+// //var clientHeight_gai = clientHeight + 20;
+// document.getElementById("page_word").style.height=clientHeight+"px";
+// document.getElementById("page_word").style.backgroundSize=clientHeight+"px";
+// var swiper2 = new Swiper('.swiper-slide', {
+//         direction: 'vertical',
+//         slidesPerView: 'auto',
+//         mousewheelControl: true,
+//         freeMode: true,
+//         autoHeight: true,
+//     });
+// document.getElementById("guitarist").style.height=clientHeight+"px";
+// //document.getElementById("guitarist").style.width=auto;
+// var swiper3 = new Swiper('.section4', {
+//         direction: 'vertical',
+//         slidesPerView: 'auto',
+//         mousewheelControl: true,
+//         autoHeight: true,
+//         freeMode: true,
+//     });
+// document.getElementById("shuhun_div").style.height=clientHeight+"px";
+// var swiper4 = new Swiper('.section5', {
+//         direction: 'vertical',
+//         slidesPerView: 'auto',
+//         mousewheelControl: true,
+//         autoHeight: true,
+//         freeMode: true,
+//     });
+// document.getElementById("yoo_div").style.height=clientHeight+"px";
+// var swiper5 = new Swiper('.section6', {
+//         direction: 'vertical',
+//         slidesPerView: 'auto',
+//         mousewheelControl: true,
+//         autoHeight: true,
+//         freeMode: true,
+//     });
+// document.getElementById("chensan_div").style.width=clientwidth+"px";
+// //document.getElementById("chensan_div").style.height=clientHeight+"px";
+// var swiper6 = new Swiper('.section7', {
+//         direction: 'vertical',
+//         slidesPerView: 'auto',
+//         mousewheelControl: true,
+//         autoHeight: true,
+//         freeMode: true,
+//     });
+// var swiper7 = new Swiper('.section8', {
+//         direction: 'vertical',
+//         slidesPerView: 'auto',
+//         mousewheelControl: true,
+//         autoHeight: true,
+//         freeMode: true,
+//     });
+// var swiper8 = new Swiper('.section9',{
+//     autoplay:3000,
+//     loop: true,
+// });
+// document.getElementById('zhuangtai_div').style.height=clientHeight+'px';
+// var swiper9 = new Swiper('.section10', {
+//         direction: 'vertical',
+//         slidesPerView: 'auto',
+//         mousewheelControl: true,
+//         autoHeight: true,
+//         freeMode: true,
+//     });
+// //document.getElementById("jiepou_div").style.height=clientHeight+"px";
+// // var swiper7 = new Swiper('.section8', {
+// //         direction: 'vertical',
+// //         slidesPerView: 'auto',
+// //         mousewheelControl: true,
+// //         autoHeight: true,
+// //         freeMode: true,
+// //     });
+// window.onload=function(){
+//     var clientwidth = document.body.clientWidth;
+//     var clientHeight = document.body.clientHeight;
+//     console.log(clientHeight);
+//     console.log(clientwidth);
+//     var screenwidth = document.body.scrollWidth;
+//     var screenHeight = document.body.scrollHeight;
+//     //document.getElementById("page_word").style.height=clientHeight+"px";
+//     //document.getElementById("page_word1").setAttribute("style","height:"+1700+"px");
+//     //document.getElementById("page_word2").setAttribute("style","height:"+1700+"px");
+//     console.log(screenHeight+500);
+//     console.log(screenHeight);
+//     console.log(screenwidth);
+//     console.log(document.body.scrollButtom);
+// };
+// var screenHeight = document.body.scrollHeight;
+var swiper1 = new Swiper('.swiper-container-horizontal', {
+         pagination: '.swiper-pagination',
+         paginationClickable: true,
+         hashnav: true,
+     });
+var swiper4 = new Swiper('.the_section1', {
+        direction: 'vertical',
+        slidesPerView: 'auto',
+        mousewheelControl: true,
+        autoHeight: true,
+        freeMode: true,
+    });
+var swiper5 = new Swiper('.the_section4', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper6 = new Swiper('.the_section5', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper7 = new Swiper('.the_section7', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper8 = new Swiper('.the_section8', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper9 = new Swiper('.the_section9', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper10 = new Swiper('.the_section10', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper11 = new Swiper('.the_section11', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper12 = new Swiper('.the_section12', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper13 = new Swiper('.the_section13', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper14 = new Swiper('.the_section14', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper15 = new Swiper('.the_section15', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper16 = new Swiper('.the_section16', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper17 = new Swiper('.the_section17', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper18 = new Swiper('.the_section18', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper16 = new Swiper('.the_section16', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper19 = new Swiper('.the_section19', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper20 = new Swiper('.the_section20', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper21 = new Swiper('.the_section21', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper22 = new Swiper('.the_section22', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper23 = new Swiper('.the_section23', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper24 = new Swiper('.the_section24', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper25 = new Swiper('.the_section25', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper26 = new Swiper('.the_section26', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper27 = new Swiper('.the_section27', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
+var swiper28 = new Swiper('.the_section28', {
+    direction: 'vertical',
+    slidesPerView: 'auto',
+    mousewheelControl: true,
+    autoHeight: true,
+    freeMode: true,
+});
